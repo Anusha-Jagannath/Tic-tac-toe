cce4dc42991b99bf691116b5d74802a27c651f63
Anusha-Jagannath <anushajaganath6@gmail.com>
2021-08-26 09:56:40 +0530

    welcome message added

---
+++ src/com/tictactoe/TicTacToeGame.java
@@ -0,0 +1,22 @@
+package com.tictactoe;
+
+public class TicTacToeGame {
+
+	public void play() {
+//		As a Player would like to
+//		start fresh by creating a tic
+//		tac toe board - Create a TicTacToeGame class - Create method for every use case
+//		and call from main
+//		- Create a board of char[] of size 10
+//		and assign empty space to each
+//		element
+		
+		char board[]=new char[10];
+		for(int i=0;i<10;i++)
+			board[i]=' ';
+        
+		
+		
+	}
+
+}
