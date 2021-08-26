ffca93703fae245328d6a08b601729f6916dcd43
Anusha-Jagannath <67367819+Anusha-Jagannath@users.noreply.github.com>
2021-08-26 09:58:36 +0530

    Update Main.java

--- src/com/tictactoe/Main.java
+++ src/com/tictactoe/Main.java
@@ -3,7 +3,9 @@ package com.tictactoe;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("Welcome to tic tac toe game");
+		TicTacToeGame game=new TicTacToeGame();
+		game.play();
 
 	}
 
